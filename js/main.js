c48bd4a68e26e95031c83189b57c0acaeeaf4042
hi-raaaaaaaaaaaaaaaaaaaaaaaaaaa <hirata.daichi.df@st.spec.ed.jp>
2025-05-31 00:16:01 +0900

    色々頑張った

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 function click_carrot() {
-    this.classList.add("click");
+    var sW = window.innerWidth;
+    var sH = window.innerHeight;
+    let start = Date.now(); // 開始時間を覚える
+    
+    const fruit = document.getElementById('carrot_1').getBoundingClientRect();
+    const fruitLeft = fruit.left + window.pageXOffset;
+    const fruitTop = fruit.top + window.pageYOffset;
+    console.warn(fruitLeft,fruitTop);
+
+    let timer = setInterval(function() {
+        // 開始からの経過時間は？
+        let timePassed = Date.now() - start;
+
+        if ((parseInt(carrot_1.style.left) > sW - 250) && (parseInt(carrot_1.style.left) > sH - 250)) {
+            clearInterval(timer); // アニメーションが終了
+            return;
+        }
+
+        // timePassed 時点のアニメーションを描画
+        drawLeft(timePassed);
+
+    }, 20);
+
+    // timePassed は 0 から 2000 まで進む
+    // なので、left は 0px から 400px になります
+    function drawLeft(timePassed) {
+        carrot_1.style.left = (timePassed / 2.5) + fruitLeft + 'px';
+        carrot_1.style.top = (timePassed / 5) + fruitTop + 'px';
+    }
 }
+

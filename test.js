8355e5c53dfbe2e2fb9b08716cbdc3bd0b66f3ae
hi-raaaaaaaaaaaaaaaaaaaaaaaaaaa <hirata.daichi.df@st.spec.ed.jp>
2025-08-07 00:38:32 +0900

    眠い寝ようおやすみ

--- test.js
+++ test.js
@@ -4,6 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const pressStart = document.getElementById('press-start');
     const gameSettings = document.getElementById('game-settings');
 
+    const downbuttonOpt = document.getElementById('down-options');
+    const upbuttonOpt = document.getElementById('up-options');
+    const textOpt = document.getElementById('textbox-options');
+
+    const downbuttonType = document.getElementById('down-type');
+    const upbuttonType = document.getElementById('up-type');
+    const textType = document.getElementById('textbox-type');
+
     // 画面がタップされたときの処理
     titleScreen.addEventListener('click', () => {
         // 背景を上方向にスライドさせるクラスを追加
@@ -16,4 +24,47 @@ document.addEventListener('DOMContentLoaded', () => {
             gameSettings.style.display = 'flex'; // ゲーム設定を表示する
         }, { once: true }); // イベントリスナーを一度だけ実行
     });
+
+    //ボタンが押されたらカウント減(個数)
+    downbuttonOpt.addEventListener('click', (event) => {
+        //1以下にはならないようにする
+        if(textOpt.value > 1) {
+            textOpt.value--;
+        }
+        if(textType.value > textOpt.value) {
+            textType.value--;
+        }
+    });
+
+    //ボタンが押されたらカウント増
+    upbuttonOpt.addEventListener('click', (event) => {
+        if(textOpt.value < 8) {
+            textOpt.value++;
+        }
+    })
+
+    //ボタンが押されたらカウント減(種類)
+    downbuttonType.addEventListener('click', (event) => {
+        //1以下にはならないようにする
+        if(textType.value > 1) {
+            textType.value--;
+        }
+    });
+
+    //ボタンが押されたらカウント増
+    upbuttonType.addEventListener('click', (event) => {
+        //選択肢の数以下にはならないようにする
+        console.warn(textOpt.value, textType.value);
+        if(textType.value < textOpt.value && textType.value < 8) {
+            textType.value++;
+        }
+    })
+
 });
+
+function getformat() {
+    const format_num = document.getElementById("format").value;
+    console.log(format_num);
+}
+
+
